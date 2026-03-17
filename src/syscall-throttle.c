6f443fdd1a1c0c60d1ed8c1d179830e835953b85
gmarseglia <g.marseglia.it@gmail.com>
2026-03-17 18:02:39 +0100

    Added first search mechanism for per-cpu probing context

--- src/syscall-throttle.c
+++ src/syscall-throttle.c
@@ -4,6 +4,7 @@
 #include <linux/atomic.h>
 #include <linux/preempt.h>
 #include <linux/irqflags.h>
+#include <linux/smp.h>
 
 #define MODNAME "SYSCALL-THROTTLE"
 
@@ -14,7 +15,9 @@
 static struct kprobe the_probe;
 static atomic_t the_counter = ATOMIC_INIT(0);
 
-static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
+DEFINE_PER_CPU(struct kprobe *, kprobe_context_position);
+
+static int __kprobes pre_handler_throttle(struct kprobe *p, struct pt_regs *regs)
 {
     // #TODO: why orig_ax works?
     struct pt_regs *the_regs = (struct pt_regs *)regs->di;
@@ -47,14 +50,73 @@ static int __kprobes handler_pre(struct kprobe *p, struct pt_regs *regs)
     return 0;
 }
 
+void dummy_run(void *arg)
+{
+    pr_info("%s: dummy running on CPU %d", MODNAME, smp_processor_id());
+    return;
+}
+
+static int __kprobes pre_handler_search(struct kprobe *p, struct pt_regs *regs)
+{
+    struct kprobe **temp = (struct kprobe **)&kprobe_context_position;
+
+    while (temp > 0)
+    {
+        temp--;
+        if ((unsigned long)__this_cpu_read(*temp) == (unsigned long)p)
+        {
+            printk("%s: found on CPU %d at %p", MODNAME, smp_processor_id(), temp);
+            break;
+        }
+    }
+
+    if (temp == 0)
+    {
+        pr_err("%s: NOT found on CPU %d", MODNAME, smp_processor_id());
+        return 0;
+    }
+
+    // struct kprobe **re_read = __this_cpu_read(*kprobe_context_position);
+
+    pr_info("%s: pre_handler_search running on CPU %d", MODNAME, smp_processor_id());
+    return 0;
+}
+
+int load_hack(void)
+{
+    int ret_search;
+    struct kprobe search_probe;
+
+    search_probe.symbol_name = "dummy_run";
+    search_probe.pre_handler = pre_handler_search;
+
+    ret_search = register_kprobe(&search_probe);
+    if (ret_search < 0)
+    {
+        pr_err("%s: register_kprobe failed, returned %d\n", MODNAME, ret_search);
+        return ret_search;
+    }
+
+    // smp_call_function(dummy_run, NULL, 1);
+    on_each_cpu(dummy_run, NULL, 1);
+
+    pr_info("%s: load_hack completed.", MODNAME);
+
+    unregister_kprobe(&search_probe);
+
+    return 0;
+}
+
 int init_module(void)
 {
     int ret;
 
     pr_info("%s: module correctly loaded\n", MODNAME);
 
+    load_hack();
+
     the_probe.symbol_name = target_func;
-    the_probe.pre_handler = handler_pre;
+    the_probe.pre_handler = pre_handler_throttle;
 
     ret = register_kprobe(&the_probe);
     if (ret < 0)

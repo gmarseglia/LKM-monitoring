6b7e81c22165b6e7ec7a9bb752e96059f3432f30
gmarseglia <g.marseglia.it@gmail.com>
2026-03-24 12:52:44 +0100

    Added manual wake up on throttle stop

--- module/src/driver.c
+++ module/src/driver.c
@@ -76,13 +76,14 @@ dev_ioctl(struct file *filp, unsigned int command, unsigned long param)
 	}
 
 	/* Execute command */
-	int ret;
+	int ret = 0;
 	switch (command) {
 	case IOCTL_START_THROTTLE:
 		atomic_set(&sys_thr_cxt->running, true);
 		break;
 	case IOCTL_STOP_THROTTLE:
 		atomic_set(&sys_thr_cxt->running, false);
+		update_limit_and_wake();
 		break;
 	case IOCTL_REGISTER_NR:
 		ret = register_critical_num(nr);

02edc725571c21a7c3c9da8e420e74b6b037d777
gmarseglia <g.marseglia.it@gmail.com>
2026-04-08 14:48:49 +0200

    Refactor: added scripts/ dir

--- scripts/retry-and-register-read.sh
+++ scripts/retry-and-register-read.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo dmesg -C
-make retry
+make -C ../module retry 
+make -C ../user all
 sudo /home/ghi/LKM-monitoring/user/ioctl.out 6 user.out
-#sudo /home/ghi/LKM-monitoring/user/ioctl.out 2 1
 sudo /home/ghi/LKM-monitoring/user/ioctl.out 2 0
 sudo /home/ghi/LKM-monitoring/user/ioctl.out 0
 
